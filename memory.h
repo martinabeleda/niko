2cdb6e63db2e45cf46cd4da8b93b843ffa1641c0
martinabeleda <martin.abeleda@gmail.com>
2024-06-07 20:17:29 -0700

    run autoformat

--- memory.h
+++ memory.h
@@ -1,85 +1,78 @@
 #pragma once
 #define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
 #include <TlHelp32.h>
+#include <Windows.h>
 #include <string_view>
 
-class Memory
-{
+class Memory {
 private:
-	std::uintptr_t processId = 0;
-	void* processHandle = nullptr;
+  std::uintptr_t processId = 0;
+  void *processHandle = nullptr;
 
 public:
-	// Constructor that finds the process id
-	// and opens a handle
-	Memory(std::string_view processName) noexcept
-	{
-		::PROCESSENTRY32 entry = { };
-		entry.dwSize = sizeof(::PROCESSENTRY32);
+  // Constructor that finds the process id
+  // and opens a handle
+  Memory(std::string_view processName) noexcept {
+    ::PROCESSENTRY32 entry = {};
+    entry.dwSize = sizeof(::PROCESSENTRY32);
 
-		const HANDLE snapShot = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    const HANDLE snapShot = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-		while (::Process32Next(snapShot, &entry))
-		{
-			if (!processName.compare(entry.szExeFile))
-			{
-				processId = entry.th32ProcessID;
-				processHandle = ::OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
-				break;
-			}
-		}
+    while (::Process32Next(snapShot, &entry)) {
+      if (!processName.compare(entry.szExeFile)) {
+        processId = entry.th32ProcessID;
+        processHandle = ::OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+        break;
+      }
+    }
 
-		// Free handle
-		if (snapShot)
-			::CloseHandle(snapShot);
-	}
+    // Free handle
+    if (snapShot)
+      ::CloseHandle(snapShot);
+  }
 
-	// Destructor that frees the opened handle
-	~Memory()
-	{
-		if (processHandle)
-			::CloseHandle(processHandle);
-	}
+  // Destructor that frees the opened handle
+  ~Memory() {
+    if (processHandle)
+      ::CloseHandle(processHandle);
+  }
 
-	// Returns the base address of a module by name
-	std::uintptr_t GetModuleAddress(std::string_view moduleName) const noexcept
-	{
-		::MODULEENTRY32 entry = { };
-		entry.dwSize = sizeof(::MODULEENTRY32);
+  // Returns the base address of a module by name
+  std::uintptr_t GetModuleAddress(std::string_view moduleName) const noexcept {
+    ::MODULEENTRY32 entry = {};
+    entry.dwSize = sizeof(::MODULEENTRY32);
 
-		const HANDLE snapShot = ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, processId);
+    const HANDLE snapShot =
+        ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, processId);
 
-		std::uintptr_t result = 0;
+    std::uintptr_t result = 0;
 
-		while (::Module32Next(snapShot, &entry))
-		{
-			if (!moduleName.compare(entry.szModule))
-			{
-				result = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
-				break;
-			}
-		}
+    while (::Module32Next(snapShot, &entry)) {
+      if (!moduleName.compare(entry.szModule)) {
+        result = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+        break;
+      }
+    }
 
-		if (snapShot)
-			::CloseHandle(snapShot);
+    if (snapShot)
+      ::CloseHandle(snapShot);
 
-		return result;
-	}
+    return result;
+  }
 
-	// Read process memory
-	template <typename T>
-	const T Read(const std::uintptr_t address) const noexcept
-	{
-		T value = { };
-		::ReadProcessMemory(processHandle, reinterpret_cast<const void*>(address), &value, sizeof(T), NULL);
-		return value;
-	}
+  // Read process memory
+  template <typename T>
+  const T Read(const std::uintptr_t address) const noexcept {
+    T value = {};
+    ::ReadProcessMemory(processHandle, reinterpret_cast<const void *>(address),
+                        &value, sizeof(T), NULL);
+    return value;
+  }
 
-	// Write process memory
-	template <typename T>
-	void Write(const std::uintptr_t address, const T& value) const noexcept
-	{
-		::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
-	}
+  // Write process memory
+  template <typename T>
+  void Write(const std::uintptr_t address, const T &value) const noexcept {
+    ::WriteProcessMemory(processHandle, reinterpret_cast<void *>(address),
+                         &value, sizeof(T), NULL);
+  }
 };
--- vector.h
+++ vector.h
@@ -1,51 +1,41 @@
 #pragma once
-#include <numbers>
 #include <cmath>
+#include <numbers>
 
-struct Vector3
-{
-	// constructor
-	Vector3(
-		const float x = 0.f,
-		const float y = 0.f,
-		const float z = 0.f) noexcept :
-		x(x), y(y), z(z) { }
+struct Vector3 {
+  // constructor
+  Vector3(const float x = 0.f, const float y = 0.f,
+          const float z = 0.f) noexcept
+      : x(x), y(y), z(z) {}
 
-	// operator overloads
-	const Vector3 operator-(const Vector3& other) const noexcept
-	{
-		return Vector3{ x - other.x, y - other.y, z - other.z };
-	}
+  // operator overloads
+  const Vector3 operator-(const Vector3 &other) const noexcept {
+    return Vector3{x - other.x, y - other.y, z - other.z};
+  }
 
-	const Vector3 operator+(const Vector3& other) const noexcept
-	{
-		return Vector3{ x + other.x, y + other.y, z + other.z };
-	}
+  const Vector3 operator+(const Vector3 &other) const noexcept {
+    return Vector3{x + other.x, y + other.y, z + other.z};
+  }
 
-	const Vector3 operator/(const float factor) const noexcept
-	{
-		return Vector3{ x / factor, y / factor, z / factor };
-	}
+  const Vector3 operator/(const float factor) const noexcept {
+    return Vector3{x / factor, y / factor, z / factor};
+  }
 
-	const Vector3 operator*(const float factor) const noexcept
-	{
-		return Vector3{ x * factor, y * factor, z * factor };
-	}
+  const Vector3 operator*(const float factor) const noexcept {
+    return Vector3{x * factor, y * factor, z * factor};
+  }
 
-	// utils
-	const Vector3 ToAngle() const noexcept
-	{
-		return Vector3{
-			std::atan2(-z, std::hypot(x, y)) * (180.0f / std::numbers::pi_v<float>),
-			std::atan2(y, x) * (180.0f / std::numbers::pi_v<float>),
-			0.0f };
-	}
+  // utils
+  const Vector3 ToAngle() const noexcept {
+    return Vector3{
+        std::atan2(-z, std::hypot(x, y)) * (180.0f / std::numbers::pi_v<float>),
+        std::atan2(y, x) * (180.0f / std::numbers::pi_v<float>), 0.0f};
+  }
 
-	const bool IsZero() const noexcept
-	{
-		return x == 0.f && y == 0.f && z == 0.f;
-	}
+  const bool IsZero() const noexcept {
+    return x == 0.f && y == 0.f && z == 0.f;
+  }
 
-	// struct data
-	float x, y, z;
+  // struct data
+  float x, y, z;
 };
